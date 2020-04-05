0ccf3c655f33cf5b1b75504b1379eaea17a64427
felipe <Felipe Loureiro>
2020-04-05 19:56:34 -0300

    first commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gcloud auth activate-service-account --key-file ./keys.json --project $PROJECT_ID
+
+gcloud --quiet config set project $PROJECT_ID
+gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
+
+gcloud compute ssh --project $PROJECT_ID --zone $CLOUDSDK_COMPUTE_ZONE $INSTANCE -- './deploy.sh'
