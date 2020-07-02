717a6cd1a13d237f2a59e9da30f460b2d8bb6924
Felipe <felipe-loureiro@outlook.com>
2020-07-01 20:18:59 -0400

    merging 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,7 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MainPageComponent } from './views/main-page/main-page.component';
-import { AuthConfirmComponent } from './views/auth-confirm/auth-confirm.component';
-import { CadastroComponent } from './views/cadastro/cadastro.component';
-import { CadastroSegundaEtapaComponent } from './views/cadastro-segunda-etapa/cadastro-segunda-etapa.component';
-import { BookPageComponent } from './views/book-page/book-page.component';
-
-const routes: Routes = [
-  {
-    path: '', component: MainPageComponent
-  },
-  {
-    path: 'book', component: BookPageComponent
-  },
-  {
-    path: 'confirm', component: AuthConfirmComponent
-  },
-  { 
-    path: 'cadastro', component: CadastroComponent
-  },
-  { 
-    path: 'cadastro/detalhes', component: CadastroSegundaEtapaComponent
-  }
-=======
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MainPageComponent} from './views/main-page/main-page.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
 
 
 const routes: Routes = [
@@ -34,10 +9,12 @@ const routes: Routes = [
         path: '', component: MainPageComponent,
     },
     {
+      path: 'cadastro', component: CadastroComponent,
+    },
+    {
         path: 'book',
         loadChildren: () => import('./views/book-page/book.module').then(m => m.BookModule)
     }
->>>>>>> 5ba1e4bab938829a95e995a7034974a8f9741b2f
 ];
 
 @NgModule({
