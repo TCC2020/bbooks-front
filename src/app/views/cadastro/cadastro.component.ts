bd9e3cbd6a2bcad2fc6587a8a22f85fd612aae2d
Luana Tavares Faiotto <luana.faiotto@gmail.com>
2020-07-08 17:06:00 -0300

    Regra de criação de senha

--- src/app/views/cadastro/cadastro.component.ts
+++ src/app/views/cadastro/cadastro.component.ts
@@ -1,8 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CadastroService } from '../../services/cadastro-service.service';
 import { Md5 } from 'ts-md5/dist/md5';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-cadastro',
@@ -28,6 +37,23 @@ export class CadastroComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  //Validação automática de e-mail
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  matcherEmail = new MyErrorStateMatcher();
+
+  //Validação automática de senha
+  pwdFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(20),
+    Validators.pattern(/^(?=\D*\d)(?=[^A-Za-z]*[A-Za-z]).{8,20}$/)
+  ])
+  matcherPwd = new MyErrorStateMatcher();
+
 
   cadastrar(){
     
