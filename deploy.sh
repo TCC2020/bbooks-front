ee23c55b8d3c2623e6eb5d8a4b0b6b7c83c9bc4f
Felipe <felipe-loureiro@outlook.com>
2020-09-09 21:28:46 -0400

    deleting files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gcloud auth activate-service-account --key-file ./travis.json --project $PROJECT_ID
-
-gcloud --quiet config set project $PROJECT_ID
-gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
-
-gcloud compute ssh --project $PROJECT_ID --zone $CLOUDSDK_COMPUTE_ZONE $INSTANCE -- './deploy.sh'
