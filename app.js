7390b4a1718760da96e59fafdc751d5c1582cca6
Felipe Loureiro <feloureiro@Unknown.Home>
2020-09-22 22:16:47 -0400

    const add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-import sslRedirect from 'heroku-ssl-redirect';
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 app.use(express.static(__dirname + '/dist/bbooks'));
