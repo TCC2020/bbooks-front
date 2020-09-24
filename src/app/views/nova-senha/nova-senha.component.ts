7125470d9fe2a2c19cffcad4a6dc42885af336fc
rafaelpeinado <rafael.peinado@gmail.com>
2020-09-24 01:29:18 -0300

    iniciado integraçao tela de nova senha

--- src/app/views/nova-senha/nova-senha.component.ts
+++ src/app/views/nova-senha/nova-senha.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-nova-senha',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaSenhaComponent implements OnInit {
 
-  constructor() { }
+  newPassword: FormGroup;
+  profileTo: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private profileService: ProfileService,
+    private authService: AuthService) {
+    
+   }
 
   ngOnInit(): void {
+    this.createForm();
+    this.profileService.getById(this.authService.getUser().profile.id).subscribe(
+      user => {
+        this.profileTo = user;
+        this.createForm();
+      }
+    );
   }
 
+  createForm() {
+    this.newPassword = this.fb.group({
+        email: [this.profileTo?.user?.email ? this.profileTo.user.email : '', Validators.compose([
+            Validators.required,
+            Validators.email
+        ])],
+    });
+  }
 }
