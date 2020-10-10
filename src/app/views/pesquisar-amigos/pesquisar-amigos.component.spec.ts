a33568bcfe92f68803c3c4531752be0e796c9679
rafaelpeinado <rafael.peinado@gmail.com>
2020-10-09 21:58:39 -0300

    arquivo spec editado

--- src/app/views/pesquisar-amigos/pesquisar-amigos.component.spec.ts
+++ src/app/views/pesquisar-amigos/pesquisar-amigos.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SocialLoginModule } from 'angularx-social-login';
+import { MaterialModule } from 'src/app/material/material.module';
+import { TranslateServiceMockForRoot } from 'src/app/mocks/translate.service.mock';
 
 import { PesquisarAmigosComponent } from './pesquisar-amigos.component';
 
 describe('PesquisarAmigosComponent', () => {
   let component: PesquisarAmigosComponent;
   let fixture: ComponentFixture<PesquisarAmigosComponent>;
+  
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PesquisarAmigosComponent ]
+      declarations: [ PesquisarAmigosComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        SocialLoginModule,
+        TranslateServiceMockForRoot
+    ],
+    providers: [
+      FormBuilder,
+    ]
     })
     .compileComponents();
   }));
