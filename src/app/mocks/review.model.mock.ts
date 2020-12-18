3dd8b06bff4f10e5c4f5ac26fdbb2b57ae2705fe
nfandre <andrelive.05@hotmail.com>
2020-12-18 01:58:32 -0300

    fix: config tests unit jest

---
+++ src/app/mocks/review.model.mock.ts
@@ -0,0 +1,11 @@
+import {ReviewTO} from '../models/ReviewTO.model';
+import {profileMock} from './profile.model.mock';
+import {of} from 'rxjs';
+
+export const reviewMock = new ReviewTO();
+reviewMock.id = 'sdfjaoiesoijfdsf';
+reviewMock.profileTO = of(profileMock);
+reviewMock.title = ' asfeoifojdsafasef';
+reviewMock.profileId = 10;
+reviewMock.body = 'fsaesfsef';
+reviewMock.idGoogleBook = 'fesafjlasoief';
--- src/app/services/review.service.spec.ts
+++ src/app/services/review.service.spec.ts
@@ -1,12 +1,20 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ReviewService} from './review.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {SocialLoginModule} from 'angularx-social-login';
 
 describe('ReviewService', () => {
     let service: ReviewService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                ]
+        });
         service = TestBed.inject(ReviewService);
     });
 
