206d02f297409ee14444e9bd746e94805d6f1ac9
nfandre <andrelive.05@hotmail.com>
2020-12-20 20:49:47 -0300

    feat: consulta cep tests

--- src/app/services/consulta-cep.service.spec.ts
+++ src/app/services/consulta-cep.service.spec.ts
@@ -1,10 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ConsultaCepService} from './consulta-cep.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {of} from 'rxjs';
+import {readingsTrackingMock} from '../mocks/tracking.model';
 
 describe('ConsultaCepService', () => {
     let service: ConsultaCepService;
+    let httpMock: HttpTestingController;
+
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -13,9 +17,86 @@ describe('ConsultaCepService', () => {
             ]
         });
         service = TestBed.inject(ConsultaCepService);
+        httpMock = TestBed.inject(HttpTestingController);
+
     });
 
-    it('should be created', () => {
+    it('should create consulta-cep service', () => {
         expect(service).toBeTruthy();
     });
+
+    it('findByCep: should call http get', done => {
+        service.findByCep('08577000')
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne('https://viacep.com.br/ws/08577000/json');
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+    it('findByCep: should observable {} with cep invalido', done => {
+        service.findByCep('085770')
+            .subscribe((result) => {
+                expect(result).toStrictEqual({});
+                done();
+            });
+    });
+
+    it('getStatesBr: should call http get', done => {
+        service.getStatesBr()
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne('assets/state.json');
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+    it('getCitysBr: should call http get', done => {
+        service.getCitysBr('1')
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne('assets/city.json');
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+    it('getStates: should call http get', done => {
+        service.getStates('1')
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne('https://secure.geonames.org/childrenJSON?geonameId=' + '1' + '&username=' + 'Bulls2020');
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+    it('getCitys: should call http get', done => {
+        service.getCitys('1')
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne('https://secure.geonames.org/childrenJSON?geonameId=' + '1' + '&username=' + 'Bulls2020');
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+
+    it('orderBy: should order items', done => {
+        const items = [
+            { name: 'fabiola'},
+            { name: 'icaro'},
+            { name: 'pedro'},
+            { name: 'wendell'},
+            { name: 'alisson'},
+            { name: 'wendell'},
+
+        ];
+        const result = service.orderBy(items);
+        expect(result[0].name).toEqual('alisson');
+        expect(result[1].name).toEqual('fabiola');
+        expect(result[2].name).toEqual('icaro');
+        expect(result[3].name).toEqual('pedro');
+        expect(result[4].name).toEqual('wendell');
+        expect(result[5].name).toEqual('wendell');
+        done();
+    });
+
 });
