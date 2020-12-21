39b641d11059c70efcb98c43f17bb7ee8f033625
nfandre <andrelive.05@hotmail.com>
2020-12-20 22:43:43 -0300

    feat: auth services tests

--- src/app/services/cdn.service.spec.ts
+++ src/app/services/cdn.service.spec.ts
@@ -1,21 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { CDNService } from './cdn.service';
+import {CDNService} from './cdn.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('CDNService', () => {
-  let service: CDNService;
+    let service: CDNService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule
-      ]
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ]
+        });
+        service = TestBed.inject(CDNService);
+
     });
-    service = TestBed.inject(CDNService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
