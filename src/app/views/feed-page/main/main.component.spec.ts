77329daa7d062f791c6a22d1c394a78c1d01b6a5
nfandre <andrelive.05@hotmail.com>
2021-01-13 22:56:02 -0300

    fix: test feed module

--- src/app/views/feed-page/main/main.component.spec.ts
+++ src/app/views/feed-page/main/main.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import {MainComponent} from './main.component';
+import {AuthService} from '../../../services/auth.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MaterialModule} from '../../../material/material.module';
+import {TranslateServiceMockForRoot} from '../../../mocks/translate.service.mock';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SocialAuthServiceConfigMock} from '../../../mocks/google.provide.mock';
+import {SocialAuthService} from 'angularx-social-login';
+import {userMock} from '../../../mocks/user.model.mock';
 
 describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+    let component: MainComponent;
+    let fixture: ComponentFixture<MainComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
-  }));
+    const authServiceMock = {
+        getUser: jest.fn(() => userMock)
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MainComponent],
+            imports: [
+                RouterTestingModule,
+                MaterialModule,
+                TranslateServiceMockForRoot,
+                HttpClientTestingModule,
+                BrowserAnimationsModule,
+            ],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: authServiceMock
+                },
+                SocialAuthServiceConfigMock,
+                SocialAuthService
+            ]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MainComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
