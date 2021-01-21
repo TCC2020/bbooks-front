34f3464fc6c0254239a75262dbc21a969cf30e94
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-01-20 23:09:21 -0300

    feat: tela grupo de leitura finalizado

--- src/app/views/reading-group/reading-group.component.spec.ts
+++ src/app/views/reading-group/reading-group.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateService, TranslateStore } from '@ngx-translate/core';
+import { MaterialModule } from 'src/app/material/material.module';
+import { TranslateServiceMockForChild } from 'src/app/mocks/translate.service.mock';
 
 import { ReadingGroupComponent } from './reading-group.component';
 
@@ -8,7 +13,17 @@ describe('ReadingGroupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReadingGroupComponent ]
+      declarations: [ ReadingGroupComponent ],
+      imports: [
+        MaterialModule,
+        HttpClientTestingModule,
+        TranslateServiceMockForChild,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        TranslateService,
+        TranslateStore
+      ]
     })
     .compileComponents();
   }));
