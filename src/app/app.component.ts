a292ca5e66adf22b4c2f2745ee03e3e1661b7900
nfandre <andrelive.05@hotmail.com>
2021-01-26 21:10:49 -0300

    feat: finish session expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './services/auth.service';
+import {BnNgIdleService} from 'bn-ng-idle';
+import {Router} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bbooks';
+
+  constructor(
+      public auth: AuthService,
+      private idleService: BnNgIdleService,
+      private router: Router,
+      private translate: TranslateService
+
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.idleService.startWatching(60)
+        .subscribe((isUserInactive) => {
+          if (isUserInactive) {
+            if ( this.auth.getUser() !== null) {
+              this.translate.get('PADRAO.SESSAO_EXPIRADA').subscribe(message => {
+                alert(message);
+              });
+              this.auth.logout();
+              this.router.navigateByUrl('/login');
+              this.idleService.resetTimer();
+            }
+          }
+        });
+  }
+
 }
