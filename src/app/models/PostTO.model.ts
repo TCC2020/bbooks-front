230bb83e93bb2e9a92b424303bde496ca3bd817c
nfandre <andrelive.05@hotmail.com>
2021-01-26 21:39:59 -0300

    feat: create models

---
+++ src/app/models/PostTO.model.ts
@@ -0,0 +1,12 @@
+import {TypePost} from './enums/TypePost.enum';
+import {PostPrivacy} from './enums/PostPrivacy.enum';
+
+export class PostTO {
+    id: number;
+    profileId: number;
+    description: string;
+    image: string;
+    tipoPost: TypePost;
+    comments: PostTO[];
+    privacy: PostPrivacy;
+}
---
+++ src/app/models/enums/PostPrivacy.enum.ts
@@ -0,0 +1,4 @@
+export enum PostPrivacy {
+    public_all = 'public',
+    friends_only = 'friend'
+}
