0177d5de175d754a9c7dc6f876b745f81489dfc0
nfandre <andrelive.05@hotmail.com>
2021-01-26 22:07:53 -0300

    feat: create service

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {PostTO} from '../models/PostTO.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+
+    api = environment.api + 'post/';
+
+    constructor(
+        private http: HttpClient,
+    ) {
+    }
+
+    save(post: PostTO): Observable<PostTO> {
+        return this.http.post<PostTO>(this.api, post);
+    }
+    update(post: PostTO): Observable<PostTO> {
+        return this.http.put<PostTO>(this.api + post.id, post);
+    }
+}
