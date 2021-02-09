9b446ff9026b75ff499b1277ea08f68b9580ed3b
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-02-09 14:29:29 -0300

    fix: arrumando lint

--- src/app/views/perfil-page/feed/feed.component.spec.ts
+++ src/app/views/perfil-page/feed/feed.component.spec.ts
@@ -16,7 +16,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import {PostService} from '../../../services/post.service';
 import {postMock, postPagination, postsMock} from '../../../mocks/post.model.mock';
-import {ReactionsModule} from "../../reactions/reactions.module";
+import {ReactionsModule} from '../../reactions/reactions.module';
 
 describe('FeedComponent', () => {
     let component: FeedComponent;
--- src/app/views/reactions/reactions.component.spec.ts
+++ src/app/views/reactions/reactions.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReactionsComponent } from './reactions.component';
-import {MaterialModule} from "../../material/material.module";
+import {MaterialModule} from '../../material/material.module';
 
 describe('ReactionsComponent', () => {
   let component: ReactionsComponent;
