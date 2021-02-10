43bc012d9ad2864f4251e3dd8cfeffb2547eb202
nfandre <andrelive.05@hotmail.com>
2021-02-09 22:00:54 -0300

    fix: feed service

--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -8,7 +8,7 @@ import {environment} from '../../environments/environment';
   providedIn: 'root'
 })
 export class FeedService {
-  api = environment.feedApi + 'feed';
+  api = environment.feedApi + 'feed/';
 
   constructor(
       private http: HttpClient
