f84af27f820685eaa836b4cddf629957dd5d9fa9
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-02-17 19:12:05 -0300

    fix: teste do public profile

--- src/app/views/public-profile-page/public-profile/public-profile.component.spec.ts
+++ src/app/views/public-profile-page/public-profile/public-profile.component.spec.ts
@@ -1,25 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PublicProfileComponent } from './public-profile.component';
+import {PublicProfileComponent} from './public-profile.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MaterialModule} from '../../../material/material.module';
 
 describe('PublicProfileComponent', () => {
-  let component: PublicProfileComponent;
-  let fixture: ComponentFixture<PublicProfileComponent>;
+    let component: PublicProfileComponent;
+    let fixture: ComponentFixture<PublicProfileComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PublicProfileComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [PublicProfileComponent],
+            imports: [
+                RouterTestingModule,
+                MaterialModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PublicProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PublicProfileComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
