fdd9cf4167173c02aae2133a922006aea0ecf5a7
nfandre <andrelive.05@hotmail.com>
2021-02-24 16:00:10 -0300

    fix: test offer view component

--- src/app/views/exchange/offer-view/offer-view.component.spec.ts
+++ src/app/views/exchange/offer-view/offer-view.component.spec.ts
@@ -7,11 +7,20 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateServiceMockForRoot} from '../../../mocks/translate.service.mock';
+import {BookService} from '../../../services/book.service';
+import {GoogleBooksService} from '../../../services/google-books.service';
+import {BookAdsService} from '../../../services/book-ads.service';
+import {UserService} from '../../../services/user.service';
+import {userMock} from '../../../mocks/user.model.mock';
+import {AuthService} from '../../../services/auth.service';
 
 describe('OfferViewComponent', () => {
     let component: OfferViewComponent;
     let fixture: ComponentFixture<OfferViewComponent>;
-
+    const authServiceMock = {
+        getUser: jest.fn(() => userMock)
+    };
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [OfferViewComponent],
@@ -22,10 +31,21 @@ describe('OfferViewComponent', () => {
                 ReactiveFormsModule,
                 BrowserModule,
                 HttpClientTestingModule,
-                BrowserAnimationsModule
+                BrowserAnimationsModule,
+                TranslateServiceMockForRoot,
+
+            ],
+            providers: [
+                BookService,
+                GoogleBooksService,
+                BookAdsService,
+                UserService,
+                {
+                    provide: AuthService,
+                    useValue: authServiceMock
+                },
             ]
-        })
-            .compileComponents();
+        }).compileComponents();
     }));
 
     beforeEach(() => {
