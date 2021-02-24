305c5b249e5c84dd5452451242b0a114cb8ae2ce
nfandre <andrelive.05@hotmail.com>
2021-02-24 16:08:43 -0300

    fix: test exchange sent and receiver

--- src/app/views/exchange/exchange-received/exchange-received.component.spec.ts
+++ src/app/views/exchange/exchange-received/exchange-received.component.spec.ts
@@ -1,16 +1,44 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ExchangeReceivedComponent} from './exchange-received.component';
+import {MaterialModule} from '../../../material/material.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateServiceMockForRoot} from '../../../mocks/translate.service.mock';
+import {ExchangeService} from '../../../services/exchange.service';
+import {AuthService} from '../../../services/auth.service';
+import {userMock} from '../../../mocks/user.model.mock';
 
 describe('ExchangeReceivedComponent', () => {
     let component: ExchangeReceivedComponent;
     let fixture: ComponentFixture<ExchangeReceivedComponent>;
-
+    const authServiceMock = {
+        getUser: jest.fn(() => userMock)
+    };
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ExchangeReceivedComponent]
-        })
-            .compileComponents();
+            declarations: [ExchangeReceivedComponent], imports: [
+                MaterialModule,
+                RouterTestingModule,
+                FormsModule,
+                ReactiveFormsModule,
+                BrowserModule,
+                HttpClientTestingModule,
+                BrowserAnimationsModule,
+                TranslateServiceMockForRoot
+            ],
+            providers: [
+                ExchangeService,
+                {
+                    provide: AuthService,
+                    useValue: authServiceMock
+                },
+            ]
+
+        }).compileComponents();
     }));
 
     beforeEach(() => {
--- src/app/views/exchange/exchange-sent/exchange-sent.component.spec.ts
+++ src/app/views/exchange/exchange-sent/exchange-sent.component.spec.ts
@@ -1,16 +1,44 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ExchangeSentComponent} from './exchange-sent.component';
+import {ExchangeService} from '../../../services/exchange.service';
+import {AuthService} from '../../../services/auth.service';
+import {userMock} from '../../../mocks/user.model.mock';
+import {MaterialModule} from '../../../material/material.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateServiceMockForRoot} from '../../../mocks/translate.service.mock';
 
 describe('ExchangeSentComponent', () => {
     let component: ExchangeSentComponent;
     let fixture: ComponentFixture<ExchangeSentComponent>;
-
+    const authServiceMock = {
+        getUser: jest.fn(() => userMock)
+    };
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ExchangeSentComponent]
-        })
-            .compileComponents();
+            declarations: [ExchangeSentComponent],
+            imports: [
+                MaterialModule,
+                RouterTestingModule,
+                FormsModule,
+                ReactiveFormsModule,
+                BrowserModule,
+                HttpClientTestingModule,
+                BrowserAnimationsModule,
+                TranslateServiceMockForRoot
+            ],
+            providers: [
+                ExchangeService,
+                {
+                    provide: AuthService,
+                    useValue: authServiceMock
+                },
+            ]
+        }).compileComponents();
     }));
 
     beforeEach(() => {
