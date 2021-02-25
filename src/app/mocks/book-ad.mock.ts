86073c638ff3dec61b8e7f526dae3dd7216978eb
nfandre <andrelive.05@hotmail.com>
2021-02-24 21:08:39 -0300

    test: mock and book ad service

---
+++ src/app/mocks/book-ad.mock.ts
@@ -0,0 +1,16 @@
+import {BookAdTO} from '../models/BookAdTO.model';
+import {BookCondition} from '../models/enums/BookCondition.enum';
+
+export const bookAdMock = new BookAdTO();
+bookAdMock.id = 'dfasefasef';
+bookAdMock.address = 'sp;itaqua;sp';
+bookAdMock.title = 'quero um livro';
+bookAdMock.condition = BookCondition.not_used;
+
+
+export const bookAdsMock = [];
+bookAdsMock.push(bookAdMock);
+bookAdsMock.push(bookAdMock);
+bookAdsMock.push(bookAdMock);
+bookAdsMock.push(bookAdMock);
+bookAdsMock.push(bookAdMock);
--- src/app/services/book-ads.service.spec.ts
+++ src/app/services/book-ads.service.spec.ts
@@ -1,10 +1,13 @@
 import {TestBed} from '@angular/core/testing';
 
 import {BookAdsService} from './book-ads.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {bookAdMock} from '../mocks/book-ad.mock';
+import {userMock} from '../mocks/user.model.mock';
 
 describe('BookAdsService', () => {
     let service: BookAdsService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -13,9 +16,65 @@ describe('BookAdsService', () => {
             ]
         });
         service = TestBed.inject(BookAdsService);
+        httpMock = TestBed.inject(HttpTestingController);
+
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('create: should call http POST', done => {
+        service.create(bookAdMock)
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne(service.api);
+        expect(req.request.method).toBe('POST');
+        done();
+    });
+
+    it('create: should call http PUT', done => {
+        service.update(bookAdMock)
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne(service.api);
+        expect(req.request.method).toBe('PUT');
+        done();
+    });
+
+    it('getAll: should call http GET', done => {
+        service.getAll()
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne(service.api);
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+    it('getAll: should call http GET', done => {
+        service.getAllByUser(userMock.id)
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne(service.api +  'user/' + userMock.id);
+        expect(req.request.method).toBe('GET');
+        done();
+    });
+
+    it('delete: should call http DELETE', done => {
+        service.delete(bookAdMock.id)
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne(service.api + bookAdMock.id);
+        expect(req.request.method).toBe('DELETE');
+        done();
+    });
+
+    it('getById: should call http DELETE', done => {
+        service.getById(bookAdMock.id)
+            .subscribe(() => {
+            });
+        const req = httpMock.expectOne(service.api + bookAdMock.id);
+        expect(req.request.method).toBe('GET');
+        done();
     });
 });
