c028c14e3df2dc9ec5fd6e2cb88e4d13ebe0ee1f
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-02-24 23:23:51 -0300

    feat: criando tela de competição

---
+++ src/app/views/literary-competition/literary-competition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-literary-competition',
+  templateUrl: './literary-competition.component.html',
+  styleUrls: ['./literary-competition.component.scss']
+})
+export class LiteraryCompetitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
