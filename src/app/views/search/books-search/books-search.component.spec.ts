fe98994041c09fecd6d73e58a5c97f00e9531417
nfandre <andrelive.05@hotmail.com>
2021-02-25 23:10:57 -0300

    fix: tests

--- src/app/views/search/books-search/books-search.component.spec.ts
+++ src/app/views/search/books-search/books-search.component.spec.ts
@@ -1,6 +1,16 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {BooksSearchComponent} from './books-search.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../../../material/material.module';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {SocialLoginModule} from 'angularx-social-login';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterTestingModule} from '@angular/router/testing';
+import {TranslateServiceMockForRoot} from '../../../mocks/translate.service.mock';
+import {SocialAuthServiceConfigMock} from '../../../mocks/google.provide.mock';
 
 describe('BooksSearchComponent', () => {
     let component: BooksSearchComponent;
@@ -8,6 +18,22 @@ describe('BooksSearchComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            imports: [
+                BrowserModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MaterialModule,
+                HttpClientTestingModule,
+                SocialLoginModule,
+                BrowserAnimationsModule,
+                NoopAnimationsModule,
+                RouterTestingModule,
+                TranslateServiceMockForRoot
+            ],
+            providers: [
+                SocialAuthServiceConfigMock
+            ],
             declarations: [BooksSearchComponent]
         })
             .compileComponents();
