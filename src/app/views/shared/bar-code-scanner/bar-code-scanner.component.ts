c0ede3de31fb738d6e9deefc7b11cd3e5cb86fa1
nfandre <andrelive.05@hotmail.com>
2021-03-05 19:21:21 -0300

    fix: enums type bar code

--- src/app/views/shared/bar-code-scanner/bar-code-scanner.component.ts
+++ src/app/views/shared/bar-code-scanner/bar-code-scanner.component.ts
@@ -3,55 +3,55 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {BehaviorSubject} from 'rxjs';
 enum BarcodeFormat {
     /** Aztec 2D barcode format. */
-    AZTEC,
+    AZTEC = 'AZTEC',
 
     /** CODABAR 1D format. */
-    CODABAR,
+    CODABAR = 'CODABAR',
 
     /** Code 39 1D format. */
-    CODE_39,
+    CODE_39 = 'CODE_39',
 
     /** Code 93 1D format. */
-    CODE_93,
+    CODE_93 = 'CODE_93',
 
     /** Code 128 1D format. */
-    CODE_128,
+    CODE_128 = 'CODE_128',
 
     /** Data Matrix 2D barcode format. */
-    DATA_MATRIX,
+    DATA_MATRIX = 'DATA_MATRIX',
 
     /** EAN-8 1D format. */
-    EAN_8,
+    EAN_8 = 'EAN_8',
 
     /** EAN-13 1D format. */
-    EAN_13,
+    EAN_13 = 'EAN_13',
 
     /** ITF (Interleaved Two of Five) 1D format. */
-    ITF,
+    ITF = 'ITF',
 
     /** MaxiCode 2D barcode format. */
-    MAXICODE,
+    MAXICODE = 'MAXICODE',
 
     /** PDF417 format. */
-    PDF_417,
+    PDF_417 = 'PDF_417',
 
     /** QR Code 2D barcode format. */
-    QR_CODE,
+    QR_CODE = 'QR_CODE',
 
     /** RSS 14 */
-    RSS_14,
+    RSS_14 = 'RSS_14',
 
     /** RSS EXPANDED */
-    RSS_EXPANDED,
+    RSS_EXPANDED = 'RSS_EXPANDED',
 
     /** UPC-A 1D format. */
-    UPC_A,
+    UPC_A = 'UPC_A',
 
     /** UPC-E 1D format. */
-    UPC_E,
+    UPC_E = 'UPC_E',
 
     /** UPC/EAN extension format. Not a stand-alone format. */
-    UPC_EAN_EXTENSION
+    UPC_EAN_EXTENSION = 'UPC_EAN_EXTENSION'
 
 }
 export default BarcodeFormat;
@@ -139,6 +139,9 @@ export class BarCodeScannerComponent implements OnInit {
 
     onTorchCompatible(isCompatible: boolean): void {
         this.torchAvailable$.next(isCompatible || false);
+    }
+    errorCatch(error): void {
+        console.log(error)
     }
 
     toggleTorch(): void {
