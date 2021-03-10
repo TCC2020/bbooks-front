8ce93ff411bbca07b28449529a49c970216f981c
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-03-10 01:31:26 -0300

    base chat model

---
+++ schema/chat.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Chat = Schema({
+    exchangeId: {
+        type: String,
+        required: true
+    },
+    messages: [
+        {
+            message: {
+                type: String,
+                required: true
+            },
+            data: {
+                type: Date,
+                required: true
+            },
+            profileSender: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+});
+
+mongoose.model('chats', Chat);
