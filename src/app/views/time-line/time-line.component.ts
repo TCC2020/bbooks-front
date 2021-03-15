fc0fb55ca9b5ba1f56849d1b9340b4a2d4514d6a
nfandre <andrelive.05@hotmail.com>
2021-03-15 18:39:20 -0300

    refactor time-line

--- src/app/views/time-line/time-line.component.ts
+++ src/app/views/time-line/time-line.component.ts
@@ -8,6 +8,7 @@ import {map, take} from 'rxjs/operators';
 import {BookCase} from '../../models/bookCase.model';
 import {Util} from '../shared/Utils/util';
 import {Book} from '../../models/book.model';
+import {Observable, Subject, zip} from 'rxjs';
 
 @Component({
     selector: 'app-time-line',
@@ -26,11 +27,10 @@ export class TimeLineComponent implements OnInit, AfterViewInit {
     size = 40;
     side = 'left';
     mobileWidthThreshold = 640;
-    lastYear = 0;
     entries = [];
     books = [];
     loading = true;
-    contIndex = -1;
+    list: Subject<any> = new Subject();
 
     constructor(
         private bookService: BookService,
@@ -40,72 +40,80 @@ export class TimeLineComponent implements OnInit, AfterViewInit {
     ) {
     }
 
-    ngOnInit(): void {
+    async ngOnInit() {
         this.getBooks();
+        this.loading = true;
+        Util.loadingScreen();
+
     }
 
     getBooks(): void {
-        this.loading = true;
-        Util.loadingScreen();
         this.userBookService.getAllByProfileTimeLine(this.authservice.getUser().profile.id)
             .pipe(
                 take(1),
                 map(userBook => {
-                    const books = [];
-                    // @ts-ignore
+                    const bookObservable = [];
                     userBook.books.forEach((realation) => {
-                        // @ts-ignore
-                        if (realation.idBookGoogle) {
-                            this.gBooksService.getById(realation.idBookGoogle)
-                                .pipe(take(1))
-                                .subscribe(book => {
-                                    const b = this.bookService.convertBookToModel(book);
-                                    b.idUserBook = realation.id;
-                                    b.status = realation.status;
-                                    b.finishDate = realation.finishDate;
-                                    books.push(b);
-                                });
+                        let id;
+                        let getById;
+                        if (!realation.idBookGoogle) {
+                            id = realation.idBook ? realation.idBook : realation.book.id;
+                            getById = this.bookService.getById(id)
+                                .pipe(
+                                    take(1),
+                                    map(b => {
+                                        b.idUserBook = realation.id;
+                                        b.status = realation.status;
+                                        b.finishDate = realation.finishDate;
+                                        return b;
+                                    })
+                                );
                         } else {
-                            const id = realation.idBook ? realation.idBook : realation.book.id;
-                            this.bookService.getById(id)
-                                .pipe(take(1))
-                                .subscribe(b => {
-                                    b.idUserBook = realation.id;
-                                    b.status = realation.status;
-                                    b.finishDate = realation.finishDate;
-                                    books.push(b);
-                                });
+                            getById = this.gBooksService.getById(realation.idBookGoogle)
+                                .pipe(
+                                    take(1),
+                                    map(book => {
+                                        const b = this.bookService.convertBookToModel(book);
+                                        b.idUserBook = realation.id;
+                                        b.status = realation.status;
+                                        b.finishDate = realation.finishDate;
+                                        return b;
+                                    })
+                                );
                         }
+                        bookObservable.push(getById);
                     });
-                    return books;
-                })
-            )
-            .pipe(take(1))
+                    return bookObservable;
+                }),
+            ).pipe(take(1))
             .subscribe(books => {
-                setTimeout(() => {
-                    this.books = books;
-                    // @ts-ignore
-                    books = books.sort((a, b) => a.finishDate > b.finishDate);
-                    books.forEach((b) => {
+                zip(
+                    ...books
+                ).pipe(
+                    take(1)
+                ).subscribe(r => {
+                    this.books = r;
+                    this.books.forEach((b) => {
                         if (b.finishDate !== null && b.finishDate !== 0 && b.finishDate) {
                             const date = new Date(b.finishDate);
                             const year = date.getFullYear();
                             this.verifyDate(year, b);
                         }
                     });
+                    this.entries = this.entries.sort((a, b) => a.header - b.header);
                     this.loading = false;
                     Util.stopLoading();
-                }, 2200);
+                });
             });
     }
 
     verifyDate(year: number, book?: Book): void {
-        if (year > this.lastYear) {
-            this.lastYear = year;
-            this.addEntry(year, book);
-            this.contIndex++;
+        const result = this.entries.find(r => r.header === year);
+        if (result) {
+            const index = this.entries.indexOf(result);
+            this.entries[index].content.push(book);
         } else {
-            this.addBookContent(this.contIndex, book);
+            this.addEntry(year, book);
         }
     }
 
@@ -114,10 +122,6 @@ export class TimeLineComponent implements OnInit, AfterViewInit {
             header: year,
             content: [book]
         });
-    }
-
-    addBookContent(i: number, book?: Book) {
-        this.entries[i].content.push(book);
     }
 
     onExpand(event, index) {
